f09bbe595c3a4bb447020ddfad682c909444abb8
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:22:05 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasutreatment/exception/TreatmentException.java
+++ src/main/java/ingjulianvega/ximic/msscasutreatment/exception/TreatmentException.java
@@ -1,15 +1,19 @@
 package ingjulianvega.ximic.msscasutreatment.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TreatmentException extends RuntimeException {
 
-    private final String code;
-
-    public TreatmentException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 }
 
