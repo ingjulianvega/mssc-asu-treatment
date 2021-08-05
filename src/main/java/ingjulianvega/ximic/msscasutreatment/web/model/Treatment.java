785cc060130ec74182667ab1f129ab5a2c58f317
Julian Vega <ingjulianvega@gmail.com>
2021-08-05 06:05:29 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasutreatment/web/model/Treatment.java
+++ src/main/java/ingjulianvega/ximic/msscasutreatment/web/model/Treatment.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -16,13 +18,21 @@ public class Treatment implements Serializable {
 
     static final long serialVersionUID = -6929603752029308934L;
 
+    @NotNull
     private UUID visitId;
+    @NotNull
     private UUID medicineId;
+    @NotBlank
     private int concentrationQuantity;
+    @NotNull
     private UUID concentrationId;
+    @NotNull
     private UUID administrationMethodId;
+    @NotBlank
     private int durationQuantity;
+    @NotNull
     private UUID durationId;
+    @NotBlank
     private String observations;
 
 }
